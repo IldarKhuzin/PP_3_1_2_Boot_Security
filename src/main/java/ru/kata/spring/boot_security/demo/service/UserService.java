3ec78af2b990d4f073d900b0ec64d11aaf092bfc
Ildar Khuzin <ildar.khuzin75@mail.ru>
2024-11-05 22:35:10 +0300

    готово7)

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,21 +1,3 @@
-//package ru.kata.spring.boot_security.demo.service;
-//
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import ru.kata.spring.boot_security.demo.entity.User;
-//import java.util.List;
-//
-//public interface UserService extends UserDetailsService {
-//
-//    List<User> getAllUsers();
-//
-//    void saveUser(User user);
-//
-//    void delete(Long id);
-//
-//    User getById(Long id);
-//
-//    User getAuthUser();
-//}
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
